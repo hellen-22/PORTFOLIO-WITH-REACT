66aac963d933933e893ba2169b691d77f5c9ad0d
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-07-25 23:44:10 +0300

    setting background image

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
+import background from "./images/banner.jpg";
+
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage : `url(${background})` }}>
       <Header />
+      <Home />
     </div>
   );
 }
