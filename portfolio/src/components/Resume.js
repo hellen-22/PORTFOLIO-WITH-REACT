81923c84544f9c3ab5d3dfcfd6ea7fe2606fe160
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-08-01 23:14:38 +0300

    resume details

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -33,7 +33,7 @@ function Resume() {
                   <em>
                     Innovative and deadline-driven Frontend and Backend
                     Developer with 2+ years of experience designing and
-                    developing backend and frontend systems using JavaScript,
+                    developing backend and frontend systems using JavaScript, Python,
                     HTML, CSS, React, Redux, Node, Express, MongoDB, and more.
                   </em>
                 </p>
@@ -53,9 +53,10 @@ function Resume() {
                   <em>Masinde Muliro University of Science and Technology</em>
                 </p>
                 <p>
-                  Qui deserunt veniam. Et sed aliquam labore tempore sed
-                  quisquam iusto autem sit. Ea vero voluptatum qui ut
-                  dignissimos deleniti nerada porti sand markend
+                  <em>
+                    CGPA: 3.8/4.0
+                  </em>
+
                 </p>
               </div>
               <div className="resume-item">
@@ -65,10 +66,9 @@ function Resume() {
                   <em>She Code Africa</em>
                 </p>
                 <p>
-                  Quia nobis sequi est occaecati aut. Repudiandae et iusto quae
-                  reiciendis et quis Eius vel ratione eius unde vitae rerum
-                  voluptates asperiores voluptatem Earum molestiae consequatur
-                  neque etlon sader mart dila
+                  Three months mentorship program
+                  <br></br>
+                  <a href="https://drive.google.com/file/d/139mIm7_CVGBcorQ1jIIVlXkER9QH0QDV/view" className="she-code-certificate" target="_blank">Certificate of completion</a>
                 </p>
               </div>
             </div>
@@ -83,20 +83,16 @@ function Resume() {
 
                 <ul>
                   <li>
-                    Lead in the design, development, and implementation of the
-                    graphic, layout, and production communication materials
+                    Lead in the development of a new backend system for ecp.
                   </li>
                   <li>
-                    Delegate tasks to the 7 members of the design team and
-                    provide counsel on all aspects of the project.{" "}
+                    Developed a new backend system for ecp.
                   </li>
                   <li>
-                    Supervise the assessment of all graphic materials in order
-                    to ensure quality and accuracy of the design
+                    Creating DRF API endpoints for ecp.
                   </li>
                   <li>
-                    Oversee the efficient use of production project budgets
-                    ranging from $2,000 - $25,000
+                    Developing the company’s Website
                   </li>
                 </ul>
               </div>
@@ -109,26 +105,18 @@ function Resume() {
 
                 <ul>
                   <li>
-                    Developed numerous marketing programs (logos,
-                    brochures,infographics, presentations, and advertisements).
+                    Creating applications with ReactJs.
                   </li>
                   <li>
-                    Managed up to 5 projects or tasks at a given time while
-                    under pressure
-                  </li>
-                  <li>
-                    Recommended and consulted with clients on the most
-                    appropriate graphic design
-                  </li>
-                  <li>
-                    Created 4+ design presentations and proposals a month for
-                    clients and account managers
+                    Responsible for creating API's with NodeJs.
                   </li>
                 </ul>
               </div>
             </div>
           </div>
+          <p>Find my detailed resume <a href="https://drive.google.com/file/d/1232_VmU4zLy8X_nqLqjxp95CSL-004-Z/view?usp=sharing" className="resume-link" target="_blank">here</a> </p>
         </div>
+        
       </section>
       <Footer />
     </>
